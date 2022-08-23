76e2c50d30c9d13e345fc4d63a04fdbeaebd498d
Jesús Yael Ortega Aguilar <47003523+jesus-yael@users.noreply.github.com>
2022-08-23 16:15:40 -0500

    v0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Cap1
+
+function pregHerido() {
+document.write('<form action="">');
+document.write('<label for="herido">¿Estás herido?</label>')
+document.write('<select name="" id="">')
+document.write('<option value="true">Sí</option>')
+document.write('<option value="false">No</option>')
+document.write('</select>')
+document.write('<button>Responder</button>')
+document.write('</form>')
+document.write('</form>')
+}
